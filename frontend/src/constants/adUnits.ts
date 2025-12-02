c21909bdae38e4b802984a1fe9e46d5a2f92dfb4
shubankare0096 <shubankar.e11223344@gmail.com>
2025-12-02 18:44:46 +0530

    Update adUnits.ts

--- frontend/src/constants/adUnits.ts
+++ frontend/src/constants/adUnits.ts
@@ -3,16 +3,16 @@ import { Platform } from 'react-native';
 // Test Ad Unit IDs from Google AdMob
 export const AdUnits = {
   banner: Platform.select({
-    android: 'ca-app-pub-3940256099942544/6300978111',
-    ios: 'ca-app-pub-3940256099942544/2934735716',
+    android: 'ca-app-pub-9801079384342918/6300978111',
+    ios: 'ca-app-pub-9801079384342918/2934735716',
   }) || '',
   interstitial: Platform.select({
-    android: 'ca-app-pub-3940256099942544/1033173712',
-    ios: 'ca-app-pub-3940256099942544/4411468910',
+    android: 'ca-app-pub-9801079384342918/1033173712',
+    ios: 'ca-app-pub-9801079384342918/4411468910',
   }) || '',
   rewarded: Platform.select({
-    android: 'ca-app-pub-3940256099942544/5224354917',
-    ios: 'ca-app-pub-3940256099942544/1712485313',
+    android: 'ca-app-pub-9801079384342918/5224354917',
+    ios: 'ca-app-pub-9801079384342918/1712485313',
   }) || '',
 };
 
